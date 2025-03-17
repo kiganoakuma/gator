cbcdd8b97187c2264a3c70f09ac4ce97dc698013
kiganoakuma <daltonhberg@icloud.com>
2025-03-17 15:01:34 -0700

    added regiser, login, reset database query command functionality

---
+++ handler_user.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/kiganoakuma/gator/internal/database"
+)
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("usage: %v <name>", cmd.Name)
+	}
+	name := cmd.Args[0]
+
+	user, err := s.db.CreateUser(context.Background(), database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name:      name,
+	})
+	if err != nil {
+		log.Fatalf("coulnt create user: %v", err)
+	}
+
+	err = s.cfg.SetUser(user.Name)
+	if err != nil {
+		log.Fatalf("couldnt set user: %v", err)
+	}
+
+	fmt.Printf("User %v was created successfully", user.Name)
+	printUser(user)
+
+	return nil
+}
+
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("usage: %s <name>", cmd.Name)
+	}
+
+	name := cmd.Args[0]
+
+	_, err := s.db.GetUser(context.Background(), name)
+	if err != nil {
+		return fmt.Errorf("couldn't find user: %w", err)
+	}
+
+	err = s.cfg.SetUser(name)
+	if err != nil {
+		return fmt.Errorf("couldn't set current user: %w", err)
+	}
+	fmt.Println("User switched successfully!")
+	return nil
+
+}
+
+func printUser(user database.User) {
+	fmt.Printf(" * ID:        %v\n", user.ID)
+	fmt.Printf(" * Name:      %v\n", user.Name)
+}
--- main.go
+++ main.go
@@ -1,30 +1,48 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"os"
 
 	"github.com/kiganoakuma/gator/internal/config"
+	"github.com/kiganoakuma/gator/internal/database"
+	_ "github.com/lib/pq"
 )
 
 type state struct {
+	db  *database.Queries
 	cfg *config.Config
 }
 
 func main() {
+
 	cfg, err := config.Read()
 	if err != nil {
 		log.Fatalf("error reading config: %v", err)
 	}
 
+	db, err := sql.Open("postgres", cfg.DBURL)
+	if err != nil {
+		log.Fatalf("error opening db: %v", err)
+	}
+
+	dbQueries := database.New(db)
+
 	programState := &state{
+		db:  dbQueries,
 		cfg: &cfg,
 	}
 
 	cmds := commands{
 		registeredCommands: make(map[string]func(*state, command) error),
 	}
+
+	cmds.register("register", handlerRegister)
+
 	cmds.register("login", handlerLogin)
+
+	cmds.register("reset", handlerReset)
 
 	if len(os.Args) < 2 {
 		log.Fatal("Usage: cli <command> [args...]")
